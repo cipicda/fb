74a4344cdfac0ed1f93c40075881d6c43314f1fb
Cocar Ciprian <Cocar Ciprian>
2024-04-17 14:09:17 +0300

    fb clone

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Topbar from "../../components/topbar/Topbar";
+import "./profile.css";
+
+const Profile = () => {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover"></div>
+            <div className="profileInfo"></div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
